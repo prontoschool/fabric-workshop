ad80a30d651f98ad26fe0390504b28411c9ec377
zkan <kan@prontomarketing.com>
2015-11-02 20:04:09 +0700

    Try local commands to run test, commit, and push to GitHub

--- fabfile.py
+++ fabfile.py
@@ -6,3 +6,16 @@ def hello():
 
 def hi(name='Kan'):
     print 'Hi ' + name
+
+
+# Local Commands
+
+from fabric.api import local, lcd
+
+
+def deploy_fizzbuzz():
+    with lcd('fizzbuzz'):
+        local('python fizzbuzz_test.py')
+        local('git add fizzbuzz.py fizzbuzz_test.py')
+        local('git commit')
+        local('git push origin master')
