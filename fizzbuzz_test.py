ea7e8f823d9f020dfa91db9c5f27dc6c3553e81b
zkan <kan@prontomarketing.com>
2015-11-02 20:01:25 +0700

    Add fizzbuzz code

---
+++ fizzbuzz_test.py
@@ -0,0 +1,40 @@
+import unittest
+
+from fizzbuzz import FizzBuzz
+
+
+class FizzBuzzTest(unittest.TestCase):
+    def setUp(self):
+        self.fizzbuzz = FizzBuzz()
+
+    def test_input_number_should_return_same_number(self):
+        result = self.fizzbuzz.take(1)
+        self.assertEqual(result, 1)
+
+        result = self.fizzbuzz.take(2)
+        self.assertEqual(result, 2)
+
+    def test_input_number_divisible_by_3_should_return_fizz(self):
+        result = self.fizzbuzz.take(3)
+        self.assertEqual(result, 'fizz')
+
+        result = self.fizzbuzz.take(6)
+        self.assertEqual(result, 'fizz')
+
+    def test_input_number_divisible_by_5_should_return_buzz(self):
+        result = self.fizzbuzz.take(5)
+        self.assertEqual(result, 'buzz')
+
+        result = self.fizzbuzz.take(10)
+        self.assertEqual(result, 'buzz')
+
+    def test_input_number_divisible_by_3_and_5_should_return_fizzbuzz(self):
+        result = self.fizzbuzz.take(15)
+        self.assertEqual(result, 'fizzbuzz')
+
+        result = self.fizzbuzz.take(30)
+        self.assertEqual(result, 'fizzbuzz')
+
+
+if __name__ == '__main__':
+    unittest.main()
